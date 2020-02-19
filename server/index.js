c78cb1c5de90be1f5f21feb7e00fc5d0a0d5aa01
Lucas Randazzo <lucas@ombustudio.com.ar>
2020-02-19 16:59:35 -0300

    Adding DIST app

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ app.use('/api/logs', logs);
 // Handle production
 if (process.env.NODE_ENV === 'production') {
   // Static folder
-  app.use(express.static(__dirname + '/public/'))
+  app.use(express.static(__dirname + '../client/dist/'))
 
   // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '../client/dist/index.html'))
 }
 
 const port = process.env.PORT || 5000;
