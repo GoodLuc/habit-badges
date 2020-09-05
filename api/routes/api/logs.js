32aa5aeea781767f0f45f3cba77b593a0c346d82
GoodLuc <lucas@ombustudio.com.ar>
2020-09-05 20:18:43 -0300

    Adding user auth...

--- api/routes/api/logs.js
+++ api/routes/api/logs.js
@@ -3,20 +3,27 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
-// Get logs
-router.get('/', async (req, res) => {
-  const logs = await getLogs();
-  res.send(await logs.find({}).toArray());
-});
+///////////////////////////////////////
+/////// L O G S ///////////////////////
+///////////////////////////////////////
 
-// Get logs
+//// Get logs
 router.get('/:user/:year/:month', async (req, res) => {
   console.log("Year: " + req.params.year + ", Month: " + req.params.month)
   const month = await getMonth(req.params.user, req.params.year, req.params.month);
   res.send(await month.toArray());
 });
+async function getMonth(user, year, month) {
+  console.log("Getting month for " + user + " on " + month + " on year " + year)
+  const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://ombu_test:Kh5MQLgsUx8nVLGE@ombu-cluster1-3qjol.mongodb.net/test?retryWrites=true&w=majority', {
+      useNewUrlParser: true
+    });
+  
+  return client.db('HeroBadge').collection('logs').find({ "user": user, "year": year, "month": month})
+}
 
-// Add logs
+//// Add logs
 router.post("/update/:user/:year/:month", async (req, res) => {
   console.log('Params are:')
   console.log(req.params)
@@ -33,15 +40,28 @@ router.post("/update/:user/:year/:month", async (req, res) => {
     console.error(error)
   }
 });
+async function getLogs() {
+  const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://ombu_test:Kh5MQLgsUx8nVLGE@ombu-cluster1-3qjol.mongodb.net/test?retryWrites=true&w=majority', {
+      useNewUrlParser: true
+    });
+  
+  return client.db('HeroBadge').collection('logs')
+}
 
-// Delete logs
+//// Delete logs
 router.delete('/:id', async (req, res) => {
   const logs = await getLogs();
   await logs.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
   res.status(200).send();
 })
 
-// New user
+
+///////////////////////////////////////
+/////// U S E R S /////////////////////
+///////////////////////////////////////
+
+//// New user
 router.post("/makeuser", async (req, res) => {
   console.log('Making user. Body is:')
   console.log(req.body)
@@ -55,26 +75,6 @@ router.post("/makeuser", async (req, res) => {
     console.error(error)
   }
 });
-
-async function getLogs() {
-  const client = await mongodb.MongoClient.connect
-    ('mongodb+srv://ombu_test:Kh5MQLgsUx8nVLGE@ombu-cluster1-3qjol.mongodb.net/test?retryWrites=true&w=majority', {
-      useNewUrlParser: true
-    });
-  
-  return client.db('HeroBadge').collection('logs')
-}
-
-async function getMonth(user, year, month) {
-  console.log("Getting month for " + user + " on " + month + " on year " + year)
-  const client = await mongodb.MongoClient.connect
-    ('mongodb+srv://ombu_test:Kh5MQLgsUx8nVLGE@ombu-cluster1-3qjol.mongodb.net/test?retryWrites=true&w=majority', {
-      useNewUrlParser: true
-    });
-  
-  return client.db('HeroBadge').collection('logs').find({ "user": user, "year": year, "month": month})
-}
-
 async function getUsers() {
   const client = await mongodb.MongoClient.connect
     ('mongodb+srv://ombu_test:Kh5MQLgsUx8nVLGE@ombu-cluster1-3qjol.mongodb.net/test?retryWrites=true&w=majority', {
@@ -83,6 +83,54 @@ async function getUsers() {
   
   return client.db('HeroBadge').collection('users')
 }
+
+//// Get user
+router.get('/user/:email', async (req, res) => {
+  const user = await getUser(req.params.email, null, 'full');
+  console.log('user got:')
+  console.log(user)
+  res.send(await user.toArray());
+});
+router.get('/checkuser/:email', async (req, res) => {
+  const user = await getUser(req.params.email, null, 'check');
+  console.log('user got:')
+  console.log(user)
+  if (user) {
+    res.sendStatus(202)
+  } else {
+    res.sendStatus(204)
+  }
+});
+//// Add logs
+router.post("/validateuser", async (req, res) => {
+  const user = await getUser(null, req.body, 'validate');
+  console.log('user got:')
+  console.log(user)
+  if (user) {
+    res.send(user)
+  } else {
+    res.sendStatus(401)
+  }
+});
+async function getUser(email, token, action) {
+  const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://ombu_test:Kh5MQLgsUx8nVLGE@ombu-cluster1-3qjol.mongodb.net/test?retryWrites=true&w=majority', {
+      useNewUrlParser: true
+    });
+  let user
+  if (email != null) {
+    user = client.db('HeroBadge').collection('users').find({ email: email })
+  } else if (token != null) {
+    user = client.db('HeroBadge').collection('users').find({ _id: token })
+  }
+  
+  if ((action === 'full')||(action === 'validate')) {
+    return user
+  } else if (action === 'check') {
+    return (await user.toArray()).length
+  }
+}
+
 
 
 module.exports = router;
