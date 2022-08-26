a777fff06949afc1f85671d110765a431daafe40
Lucas Randazzo <lucas@skytree.live>
2022-08-26 13:57:13 -0300

    Updated some sass module versions.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,204 +1,339 @@
 <template>
   <div id="app">
     <w-app>
-			<div id="nav">
-				<div :class="['container flex align-center',{ center: centerLogo }]">
-					<figure id="logo"><img src="@/assets/icons/wings.png" alt="Habit Hero Badge"></figure> <h1>Habit Hero Badge</h1>
-					<a v-if="user" @click="menuActive = true" id="toggl" href="#"><figure><img src="@/assets/icons/menu.svg" alt="Menu"></figure></a>
-					<div :class="['flex grow',{ overlay: menuActive, show: menuActive }]" id="menu" v-if="user">
-						<router-link @click="menuActive = false" to="/">Dashboard</router-link>
-						<router-link @click="menuActive = false" to="/monthly">Monthly view</router-link>
-						<router-link @click="menuActive = false" to="/habits">Habits</router-link>
-						<router-link @click="menuActive = false" to="/rewards">Rewards</router-link>
-						<!-- <router-link @click="menuActive = false" to="/powerups">Power-Ups</router-link> -->
-						<!--router-link to="/trinkets">Trinkets</router-link>
+      <div id="nav">
+        <div :class="['container flex align-center', { center: centerLogo }]">
+          <figure id="logo"><img src="@/assets/icons/wings.png" alt="Habit Hero Badge" /></figure>
+          <h1>Habit Hero Badge</h1>
+          <a v-if="user" @click="menuActive = true" id="toggl" href="#"
+            ><figure><img src="@/assets/icons/menu.svg" alt="Menu" /></figure
+          ></a>
+          <div :class="['flex grow', { overlay: menuActive, show: menuActive }]" id="menu" v-if="user">
+            <router-link @click="menuActive = false" to="/">Dashboard</router-link>
+            <router-link @click="menuActive = false" to="/monthly">Monthly view</router-link>
+            <router-link @click="menuActive = false" to="/habits">Habits</router-link>
+            <router-link @click="menuActive = false" to="/rewards">Rewards</router-link>
+            <!-- <router-link @click="menuActive = false" to="/powerups">Power-Ups</router-link> -->
+            <!--router-link to="/trinkets">Trinkets</router-link>
 						<router-link to="/castle">Your castle</router-link-->
-						<a @click="logout" class="logout">Logout</a>
-						<figure class="close" @click="menuActive = false"><img src="/assets/icons/close.svg" alt="Close"></figure>
-					</div>
-				</div>
-			</div>
-			<router-view/>
-			<Links />
-		</w-app>
+            <a @click="logout" class="logout">Logout</a>
+            <figure class="close" @click="menuActive = false"><img src="/assets/icons/close.svg" alt="Close" /></figure>
+          </div>
+        </div>
+      </div>
+      <router-view />
+      <Links />
+    </w-app>
   </div>
 </template>
 
 <script>
-import Links from '@/components/Links.vue'
+import Links from "@/components/Links.vue";
 
 import { mapState } from "vuex";
 export default {
   data() {
     return {
-      menuActive: false
-    }
+      menuActive: false,
+    };
   },
-	components: { Links },
+  components: { Links },
   computed: {
-    ...mapState(["user","centerLogo"]),
+    ...mapState(["user", "centerLogo"]),
   },
   methods: {
-    logout: function() {
-      this.menuActive = false
-      this.$store.dispatch('unsetUser')
-      this.$store.dispatch('setBadges', {})
-      this.$store.dispatch('clearData')
-      localStorage.clear()
-      this.$router.push('/login')
+    logout: function () {
+      this.menuActive = false;
+      this.$store.dispatch("unsetUser");
+      this.$store.dispatch("setBadges", {});
+      this.$store.dispatch("clearData");
+      localStorage.clear();
+      this.$router.push("/login");
     },
   },
   mounted() {
     if (localStorage.getItem("user") !== null) {
-      let user = JSON.parse(localStorage.getItem("user"))
-      this.$store.dispatch('setUser', user)
-      if (user.habits !== undefined) { this.$store.dispatch('setBadges', user.habits) }
+      let user = JSON.parse(localStorage.getItem("user"));
+      this.$store.dispatch("setUser", user);
+      if (user.habits !== undefined) {
+        this.$store.dispatch("setBadges", user.habits);
+      }
     }
-    this.$store.dispatch('setCurrentDate')
-    this.$store.dispatch('getMonth')
+    this.$store.dispatch("setCurrentDate");
+    this.$store.dispatch("getMonth");
   },
-}
+};
 </script>
 
 <style lang="scss">
 @import "~@/scss/_resets-utils.scss";
 @import "~@/scss/_badges.scss";
+@import "~@/scss/_variables.scss";
+@import "~@/scss/_fonts.scss";
+
 html {
   scroll-behavior: smooth;
 }
 #nav {
-	padding: .3rem;
+  padding: 0.3rem;
   background: $gold;
-  h1 { 
-		font-size: 1rem; margin: 0 2rem 0 0; color: white; 
-		text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.48);
-	}
+  h1 {
+    font-size: 1rem;
+    margin: 0 2rem 0 0;
+    color: white;
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.48);
+  }
   a {
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.48);
-    font-weight: bold; margin-right: 0.714rem;
-    padding: 1.3rem; text-decoration: none; color: white; 
-		border-radius: .314rem;
-    &.router-link-exact-active { background: $azure; box-shadow: inset 0px 0px 6px 0px #545454; }
+    font-weight: bold;
+    margin-right: 0.714rem;
+    padding: 1.3rem;
+    text-decoration: none;
+    color: white;
+    border-radius: 0.314rem;
+    &.router-link-exact-active {
+      background: $azure;
+      box-shadow: inset 0px 0px 6px 0px #545454;
+    }
   }
   .container {
-    .logout { margin-left: auto; }
+    .logout {
+      margin-left: auto;
+    }
   }
-  #logo, #toggl {
-    /* filter: invert(1); width: 3.57rem; */ margin-right: 0.714rem; flex-shrink: 0;
+  #logo,
+  #toggl {
+    /* filter: invert(1); width: 3.57rem; */
+    margin-right: 0.714rem;
+    flex-shrink: 0;
   }
-	#logo { width: 4rem; }
-  #toggl { display: none; margin-left: auto; width: 2.57rem; padding: 0; filter: invert(100%) sepia(14%) saturate(7494%) hue-rotate(298deg) brightness(113%) contrast(112%); }
-  .close { display: none; }
-}
-.container.center { justify-content: center; }
-
-.button-add { 
-	height: 5rem; width: 14rem; color: white; 
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin-top: auto;
-  figure { background: none; box-shadow: none; filter: invert(98%) sepia(0%) saturate(7493%) hue-rotate(213deg) brightness(103%) contrast(104%); width: 2rem; margin-left: -.4rem; display: flex; align-items: center; img { max-height: 100%; } }
-	figcaption { margin: .3rem 0 0 .8rem; padding: 0; }
-	@media (max-width: 500px) {
-		width: auto;
-		figure { margin: auto; }
-	}
-}
-
-.overlay { 
-  position: fixed; top: 0; left: 0; bottom: 0; z-index: 3; width: 100%;
-  min-height: 100vh; flex-direction: column; background: $bg2; overflow: hidden;
-  &.show { display: flex; }
-  > div { overflow-y: auto; height: 100%; }
+  #logo {
+    width: 4rem;
+  }
+  #toggl {
+    display: none;
+    margin-left: auto;
+    width: 2.57rem;
+    padding: 0;
+    filter: invert(100%) sepia(14%) saturate(7494%) hue-rotate(298deg) brightness(113%) contrast(112%);
+  }
   .close {
-    background: $background; position: fixed; top: 0; right: 0; z-index: 10;
-    width: 5.71rem; height: 5.71rem; cursor: pointer; padding: 1rem; box-sizing: border-box;
-    img { filter: invert(1); }
-		@media (max-width: 1000px) {
-			width: 3.5rem; height: 3.5rem;
-		}
+    display: none;
+  }
+}
+.container.center {
+  justify-content: center;
+}
+
+.button-add {
+  height: 5rem;
+  width: 14rem;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: auto;
+  figure {
+    background: none;
+    box-shadow: none;
+    filter: invert(98%) sepia(0%) saturate(7493%) hue-rotate(213deg) brightness(103%) contrast(104%);
+    width: 2rem;
+    margin-left: -0.4rem;
+    display: flex;
+    align-items: center;
+    img {
+      max-height: 100%;
+    }
+  }
+  figcaption {
+    margin: 0.3rem 0 0 0.8rem;
+    padding: 0;
+  }
+  @media (max-width: 500px) {
+    width: auto;
+    figure {
+      margin: auto;
+    }
+  }
+}
+
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 3;
+  width: 100%;
+  min-height: 100vh;
+  flex-direction: column;
+  background: $bg2;
+  overflow: hidden;
+  &.show {
+    display: flex;
+  }
+  > div {
+    overflow-y: auto;
+    height: 100%;
+  }
+  .close {
+    background: $background;
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 10;
+    width: 5.71rem;
+    height: 5.71rem;
+    cursor: pointer;
+    padding: 1rem;
+    box-sizing: border-box;
+    img {
+      filter: invert(1);
+    }
+    @media (max-width: 1000px) {
+      width: 3.5rem;
+      height: 3.5rem;
+    }
   }
 }
 
 @media (max-width: 740px) {
-  #nav #toggl { display: block; }
+  #nav #toggl {
+    display: block;
+  }
   #menu {
     display: none;
     background: $bg3;
     &.show {
       display: flex;
-      .close { display: block; height: 3.7rem; width: 3.7rem; }
-      a { text-align: center; width: 100%; }
+      .close {
+        display: block;
+        height: 3.7rem;
+        width: 3.7rem;
+      }
+      a {
+        text-align: center;
+        width: 100%;
+      }
     }
   }
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 .icon-content {
-  position: relative; width: 40px; height: 40px;
+  position: relative;
+  width: 40px;
+  height: 40px;
   figcaption {
-    position: absolute; top: 0; left: 0; color: white; font-size: 1.3rem; font-weight: bold; 
-    width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; text-align: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: white;
+    font-size: 1.3rem;
+    font-weight: bold;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
   }
 }
 
-h3.dayTitle { 
-	font-size: 1.5rem; text-shadow: $textshadow; 
-	span { font-weight: 300; }
+h3.dayTitle {
+  font-size: 1.5rem;
+  text-shadow: $textshadow;
+  span {
+    font-weight: 300;
+  }
 }
 
 .box {
-  background: $shine3; padding: 1.5rem;
+  background: $shine3;
+  padding: 1.5rem;
   border-radius: 15px;
   margin-bottom: 2rem;
-  .grid { margin-bottom: 2rem; }
-  .badge, .badge-check, .edit {
-    width: 100%; box-sizing: border-box; 
+  .grid {
+    margin-bottom: 2rem;
   }
-  @media (max-width: 770px) { padding: 1rem; }
-  @media (max-width: 520px) { padding: .8rem; }
+  .badge,
+  .badge-check,
+  .edit {
+    width: 100%;
+    box-sizing: border-box;
+  }
+  @media (max-width: 770px) {
+    padding: 1rem;
+  }
+  @media (max-width: 520px) {
+    padding: 0.8rem;
+  }
 }
 
-
-
 .currency {
-	margin-top: .5rem;
-	display: flex; align-items: center;
-	.coins, .rubys {
-		display: flex; flex-direction: row-reverse;	align-items: center; margin-bottom: .2rem;
-		img { width: 2rem; height: 2rem; margin-left: .2rem; }
-	}
+  margin-top: 0.5rem;
+  display: flex;
+  align-items: center;
+  .coins,
+  .rubys {
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    margin-bottom: 0.2rem;
+    img {
+      width: 2rem;
+      height: 2rem;
+      margin-left: 0.2rem;
+    }
+  }
 }
 
 .dash {
-	h1 { 
-		font-size: 3.429rem;
-		font-weight: 300; margin-top: 4rem;
-	}
-	h2 { margin: 4rem 0 1rem; font-size: 1.714rem; font-weight: 400; }
-	@media (max-width: 700px) { 
-		h1 { font-size: 2.5rem; }
-		h2 { font-size: 1.5rem;}
-	}
+  h1 {
+    font-size: 3.429rem;
+    font-weight: 300;
+    margin-top: 4rem;
+  }
+  h2 {
+    margin: 4rem 0 1rem;
+    font-size: 1.714rem;
+    font-weight: 400;
+  }
+  @media (max-width: 700px) {
+    h1 {
+      font-size: 2.5rem;
+    }
+    h2 {
+      font-size: 1.5rem;
+    }
+  }
 }
 
 .reward-list {
-	article {
-		padding: 1rem;
-		display: flex; justify-content: space-between; align-items: center;
-		background: white;
-		border-radius: 6px;
-		box-shadow: 0px 4px 3px 0px #CFCFCF8A;
-		figure { width: 3rem; flex-shrink: 0; min-height: 3rem; }
-	}
+  article {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: white;
+    border-radius: 6px;
+    box-shadow: 0px 4px 3px 0px #cfcfcf8a;
+    figure {
+      width: 3rem;
+      flex-shrink: 0;
+      min-height: 3rem;
+    }
+  }
 }
 .price {
-	background: url('~@/assets/icons/ruby.png') center/contain no-repeat;
-	display: flex; justify-content: center; align-items: center;
-	color: white; font-weight: 600; font-size: 1.3rem;
-	flex-shrink: 0;
-	width: 3rem; height: 3rem;
+  background: url("~@/assets/icons/ruby.png") center/contain no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-weight: 600;
+  font-size: 1.3rem;
+  flex-shrink: 0;
+  width: 3rem;
+  height: 3rem;
 }
-
 </style>
--- app/src/PostService.js
+++ app/src/PostService.js
@@ -1,28 +1,26 @@
-import axios from 'axios'
-const url = 'http://ec2-13-58-174-58.us-east-2.compute.amazonaws.com/api/'
+import axios from "axios";
+const url = "http://ec2-13-58-174-58.us-east-2.compute.amazonaws.com/api/";
 //const url = 'http://localhost:5000/api/logs/'
 
 class PostService {
-
   ///////////////////////////////////////
   /////// L O G S ///////////////////////
   ///////////////////////////////////////
-  
+
   //// Get Month
   static async getMonth(load) {
     try {
-      const res = await axios.post(url+'getmonth/', {load})
-      return res.data
+      const res = await axios.post(url + "getmonth/", { load });
+      return res.data;
     } catch (err) {
-      return err
+      return err;
     }
   }
 
   //// Update Month Log
   static updateLog(load) {
-    return axios.post(url+'update/', {load})
+    return axios.post(url + "update/", { load });
   }
-
 
   ///////////////////////////////////////
   /////// U S E R S /////////////////////
@@ -30,102 +28,101 @@ class PostService {
 
   //// Create User
   static makeUser(user) {
-    return axios.post(url+'makeuser', { user })
+    return axios.post(url + "makeuser", { user });
   }
 
   // Check user (to see if exists)
   static async checkUser(email) {
     try {
-      const res = await axios.get(url+'checkuser/'+email)
-      return res.status
+      const res = await axios.get(url + "checkuser/" + email);
+      return res.status;
     } catch (err) {
-      return err
+      return err;
     }
   }
 
   // Validate user (returns full user data)
   static async validateUser(data) {
-    const res = await axios.post(url+'validateuser/', data)
+    const res = await axios.post(url + "validateuser/", data);
     try {
-      return res.data
+      return res.data;
     } catch (err) {
-      return err
+      return err;
     }
   }
 
-	// Get user data by token id
+  // Get user data by token id
   static async getUserData(data) {
-    const res = await axios.post(url+'getuserdata/', data)
+    const res = await axios.post(url + "getuserdata/", data);
     try {
-      return res.data
+      return res.data;
     } catch (err) {
-      return err
+      return err;
     }
   }
 
   // Get user points
   static async getUserPoints(user) {
-    const res = await axios.post(url+'getpoints/', { user })
+    const res = await axios.post(url + "getpoints/", { user });
     try {
-      return res.data
+      return res.data;
     } catch (err) {
-      return err
+      return err;
     }
   }
 
   ///////////////////////////////////////
   /////// I C O N S  ////////////////////
   ///////////////////////////////////////
-  
+
   //// Get Icons (search)
   static async getIcons(term) {
-    console.log('getting... '+term)
-    const res = await axios.get(url+'icons/'+term)
+    console.log("getting... " + term);
+    const res = await axios.get(url + "icons/" + term);
     try {
-      console.log('Success')
-      console.log(res)
-      return res
+      console.log("Success");
+      console.log(res);
+      return res;
     } catch (err) {
-      console.log('Error')
-      return err
+      console.log("Error");
+      return err;
     }
   }
   //// Get Icon (single)
   static async getIcon(id) {
-    console.log('getting ID... '+id)
-    const res = await axios.get(url+'icon/'+id)
+    console.log("getting ID... " + id);
+    const res = await axios.get(url + "icon/" + id);
     try {
-      console.log('Success')
-      return res
+      console.log("Success");
+      return res;
     } catch (err) {
-      console.log('Error')
-      return err
+      console.log("Error");
+      return err;
     }
   }
 
   ///////////////////////////////////////
   /////// B A D G E S  //////////////////
   ///////////////////////////////////////
-  
+
   //// Save Badge / Habit
   static saveBadge(load) {
-    return axios.post(url+'savebadge/', {load})
+    return axios.post(url + "savebadge/", { load });
   }
 
-	///////////////////////////////////////
+  ///////////////////////////////////////
   /////// R E W A R D S  ////////////////
   ///////////////////////////////////////
-  
+
   //// Save Reward
   static saveReward(load) {
-    return axios.post(url+'savereward/', {load})
+    return axios.post(url + "savereward/", { load });
   }
 
   //// Charge tab
   static chargeTab(load) {
-    return axios.post(url+'updatetabs/', {load})
+    return axios.post(url + "updatetabs/", { load });
   }
 }
 
-
-export default PostService
+export default PostService;
